791c4d80b19d3bc0c619e2c59cdd0212c56361ec
sunil dandwate <sunildandvate7@gmail.com>
2015-01-26 12:42:04 +0530

    after form add

---
+++ product_master.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<title>Product Master</title>
+</head>
+<body>
+	<form class="form-horizontal">
+	  	<div class="form-group form-group-md">
+		    <label class="col-sm-2 control-label" for="productName">Product Name</label>
+		    <div class="col-sm-3">
+		      <input class="form-control" type="text" id="productName" placeholder="Product Name">
+		    </div>
+	  	</div>
+	  	<div class="form-group form-group-md">
+		    <label class="col-sm-2 control-label" for="">&nbsp;</label>
+	    	<div class="col-sm-3 col-md-offset-1">
+	    	  	<input class="btn btn-default" value="Submit" type="submit">
+	    	</div>
+	  	</div>
+	</form>
+</body>
+</html>
